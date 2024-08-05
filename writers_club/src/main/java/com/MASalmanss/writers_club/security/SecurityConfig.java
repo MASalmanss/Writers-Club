d835d996d81ee11c6e1eb45029d32205d80afaf4
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-08-05 14:12:02 +0300

    Security filter added

--- writers_club/src/main/java/com/MASalmanss/writers_club/security/SecurityConfig.java
+++ writers_club/src/main/java/com/MASalmanss/writers_club/security/SecurityConfig.java
@@ -12,12 +12,14 @@ import org.springframework.security.config.annotation.web.configurers.HttpBasicC
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -26,7 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authoriza-> authoriza.anyRequest().permitAll())
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore();
+                .addFilterBefore(jwtAuthenticationFilter , UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
